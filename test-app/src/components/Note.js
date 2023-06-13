cda0fdddccd46b2e1867702dc5a2e99d4a2d308e
cats256 <59489624+cats256@users.noreply.github.com>
2023-06-13 17:53:49 -0500

    Move files around and make minor changes

--- test-app/src/components/Note.js
+++ test-app/src/components/Note.js
@@ -1,3 +1,7 @@
-const Note = ({ note }) => <li>{note.content}</li>
+const Note = ({ note, toggleImportance }) => {
+    const label = note.important ? 'make not important' : 'make important'
+  
+    return <li className="note">{note.content} <button onClick={toggleImportance}>{label}</button></li>
+}
 
 export default Note
