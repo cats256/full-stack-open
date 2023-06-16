470e44faa01529c9f9a87af1d3d225761fe227bd
cats256 <59489624+cats256@users.noreply.github.com>
2023-06-16 13:19:25 -0500

    Reformat phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,35 +1,48 @@
-import { useState, useEffect } from 'react'
-import personService from './services/persons'
-import Filter from './components/Filter'
-import PersonForm from './components/PersonForm'
-import Persons from './components/Persons'
-import Notification from './components/Notification'
+import { useState, useEffect } from "react";
+import personService from "./services/persons";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import Notification from "./components/Notification";
 
 const App = () => {
-  const [message, setMessage] = useState(null)
-  const [status, setStatus] = useState(null)
-  const [filter, setFilter] = useState('')
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [message, setMessage] = useState(null);
+  const [status, setStatus] = useState(null);
+  const [filter, setFilter] = useState("");
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   useEffect(() => {
-    personService
-      .getAll()
-      .then(initialPersons => setPersons(initialPersons))
-  }, [])
-  
+    personService.getAll().then((initialPersons) => setPersons(initialPersons));
+  }, []);
+
   return (
     <>
       <h2>Phonebook</h2>
       <Notification message={message} status={status} />
       <Filter filter={filter} setFilter={setFilter} />
       <h2>Add a new name and number</h2>
-      <PersonForm persons={persons} setPersons={setPersons} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} setMessage={setMessage} setStatus={setStatus}/>
+      <PersonForm
+        persons={persons}
+        setPersons={setPersons}
+        newName={newName}
+        setNewName={setNewName}
+        newNumber={newNumber}
+        setNewNumber={setNewNumber}
+        setMessage={setMessage}
+        setStatus={setStatus}
+      />
       <h2>Numbers</h2>
-      <Persons persons={persons} setPersons={setPersons} filter={filter} setMessage={setMessage} setStatus={setStatus}/>
+      <Persons
+        persons={persons}
+        setPersons={setPersons}
+        filter={filter}
+        setMessage={setMessage}
+        setStatus={setStatus}
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
