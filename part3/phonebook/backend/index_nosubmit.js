e4033da36c298bc48164daab3876d878d637afc5
cats256 <59489624+cats256@users.noreply.github.com>
2023-06-25 21:26:21 -0500

    Make minor modifications

---
+++ part3/phonebook/backend/index_nosubmit.js
@@ -0,0 +1,98 @@
+const express = require('express')
+const app = express()
+
+require('dotenv').config()
+
+const Person = require('./models/person')
+
+app.use(express.static('build'))
+app.use(express.json())
+
+app.get('/info', (req, res) => {
+  Person.find({}).then((people) => {
+    const info = `
+    <p>Phonebook has info for ${people.length} people</p>
+    <p>${new Date()}</p>
+    `
+    res.send(info)
+  })
+})
+
+app.get('/api/persons', (req, res) => {
+  Person.find().then((people) => {
+    res.json(people)
+  })
+})
+
+app.post('/api/persons', (req, res, next) => {
+  const person = new Person({ name: req.body.name, number: req.body.number })
+
+  person
+    .save()
+    .then((person) => {
+      res.json(person)
+    })
+    .catch((error) => next(error))
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).json({ error: 'missing person' })
+      }
+    })
+    .catch((error) => next(error))
+})
+
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.status(204).end()
+    })
+    .catch((error) => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const person = {
+    name: req.body.name,
+    number: req.body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+    .then((person) => {
+      res.json(person)
+    })
+    .catch((error) => next(error))
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ template/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-alert': 'off',
+    'react/prop-types': 0,
+  },
+};
