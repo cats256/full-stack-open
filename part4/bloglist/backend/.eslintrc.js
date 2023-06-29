993636a2ade8db3999e9a0c7e98dc8d8e9dfee78
cats256 <59489624+cats256@users.noreply.github.com>
2023-06-29 12:08:34 -0500

    Finish exercise 4.1-4.2

---
+++ part4/bloglist/backend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-alert': 0,
+    'react/prop-types': 0,
+    'no-console': 0,
+    'no-underscore-dangle': ['error', { allow: ['_id', '__v'] }],
+    'no-param-reassign': 0,
+  },
+};
---
+++ part4/bloglist/backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+
+const app = express();
+
+mongoose.connect(config.MONGODB_URI);
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
